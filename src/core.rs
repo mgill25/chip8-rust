c9b8dafbabca717c7cde29bc84300cb061e89558
Darshit Shah <git@darnir.net>
2019-07-11 13:13:05 +0200

    impl Debug for Machine

--- src/core.rs
+++ src/core.rs
@@ -1,21 +1,45 @@
 use std::fs::File;
 use std::io::Read;
+use std::fmt;
 
 const MEMORY_SIZE: usize = 4096;
 const STACK_SIZE: usize = 16;
 const REGISTER_COUNT: usize = 16;
 const PROGRAM_OFFSET: usize = 512;
 
+struct Memory {
+    mem: [u8; MEMORY_SIZE],
+}
+
+impl fmt::Debug for Memory {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        const ZERO: u8 = 0;
+        write!(f, "{{ ");
+        for (index, byte) in self.mem.iter().enumerate() {
+            if *byte != ZERO {
+                write!(f, "{}: {}, ", index, byte);
+            }
+        };
+        write!(f, "}}")
+    }
+}
+
 pub struct Machine {
     name: String,
     counter: u16,
     stack_ptr: u8,
-    mem: [u8; MEMORY_SIZE],
+    mem: Memory,
     stack: [u16; STACK_SIZE],
     v: [u8; REGISTER_COUNT],        // registers: v0 to vf
     i: u16,                         // "There is also a 16-bit register called I."
     delay_register: u8,
     sound_register: u8,
+}
+
+impl fmt::Debug for Machine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {{ \n\tPC: {}, \n\tSP: {}, \n\tStack: {:?}, \n\tRegisters: {:?}, \n\ti: {}, \n\tDR: {}, \n\tSR: {}, \n\tMem: {:?} }}", self.name, self.counter, self.stack_ptr, self.stack, self.v, self.i, self.delay_register, self.sound_register, self.mem)
+    }
 }
 
 impl Machine {
@@ -24,7 +48,7 @@ impl Machine {
             name: name.to_string(),
             counter: 0,
             stack_ptr: 0,
-            mem: [0; MEMORY_SIZE],
+            mem: Memory {mem: [0; MEMORY_SIZE]},
             stack: [0; STACK_SIZE],
             v: [0; REGISTER_COUNT],
             i: 0,
@@ -46,7 +70,7 @@ impl Machine {
 
         // Copy the buffer into the VM memory
         // TODO: Why not copy directly without the intermediate buffer
-        self.mem[PROGRAM_OFFSET..].clone_from_slice(&buffer);
+        self.mem.mem[PROGRAM_OFFSET..].clone_from_slice(&buffer);
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::env;
-
 mod core;
 
 fn main() {
     let rom_file = env::args().nth(1).expect("Please input a ROM file");
     let vm = core::Machine::new("Chip8", &rom_file);
+    println!("{:#?}", vm);
 }
