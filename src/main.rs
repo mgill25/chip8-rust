1057cccdb9d349c5e0e28df44ae6a1feb6d148c2
Darshit Shah <git@darnir.net>
2019-07-12 15:42:06 +0200

    Add basic logging support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#[macro_use] extern crate log;
+extern crate env_logger;
+
 use std::env;
 mod core;
 
 fn main() {
+    env_logger::init();
     let rom_file = env::args().nth(1).expect("Please input a ROM file");
     let vm = core::Machine::new("Chip8", &rom_file);
-    println!("{:#?}", vm);
+    debug!("{:#?}", vm);
 }
