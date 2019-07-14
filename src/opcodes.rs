7a323942949e03456bddc9a2ca7b42eef14df54f
Manish <manish.gill@wingify.com>
2019-07-14 12:32:14 +0530

    Removed redundant bitmask functions

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,3 +1,4 @@
+use crate::bitmasks::*;
 use std::convert::TryFrom;
 
 type Address = u16;
@@ -122,34 +123,4 @@ impl TryFrom<u16> for Instruction {
             _ => Err(format!("opcode {:X} is invalid", opcode)),
         }
     }
-}
-
-#[allow(non_snake_case)]
-fn mask_F000(opcode: u16) -> u8 {
-    ((opcode >> 12) & 0xF) as u8
-}
-
-#[allow(non_snake_case)]
-fn mask_00FF(opcode: u16) -> u8 {
-    (opcode & 0xFF) as u8
-}
-
-#[allow(non_snake_case)]
-fn mask_0F00(opcode: u16) -> u8 {
-    ((opcode >> 8) & 0xF) as u8
-}
-
-#[allow(non_snake_case)]
-fn mask_0FFF(opcode: u16) -> u16 {
-    opcode & 0xFFF
-}
-
-#[allow(non_snake_case)]
-fn mask_00F0(opcode: u16) -> u8 {
-    ((opcode >> 4) & 0xF) as u8
-}
-
-#[allow(non_snake_case)]
-fn mask_000F(opcode: u16) -> u8 {
-    (opcode & 0xF) as u8
 }
